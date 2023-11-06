c841160597c509b240b92712b20d35c15bcd0ed4
Souad <220113@fppiramide.com>
2023-11-06 08:54:45 +0100

    initial

---
+++ src/test/java/com/tresEnRaya/tresEnRaya/TresEnRayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tresEnRaya.tresEnRaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TresEnRayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
